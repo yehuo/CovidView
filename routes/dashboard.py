b143327f0d76a09f17c69d7809090d04070200b4
yehuo <2497473001@qq.com>
2020-04-23 17:44:57 +0800

    finish statis chart

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -55,25 +55,59 @@ def statis(name):
 def getPieData():
     countryName = request.form["name"]
     print(countryName)
-    x = Country().one(name=countryName)
-    print(countryName)
-    x = x.APIType
-    if x == 'basic':
-        result = ChinaStatus.one(country=countryName)
-    elif x == 'foreign':
-        result = GlobalStatus().one(country=countryName)
-    results = [
-        {"value": int(result.heal), "name": 'heal'},
-        {"value": int(result.nowConfirm), "name": 'nowConfirm', },
-        {"value": int(result.dead), "name": 'dead'},
-    ]
-    print(results)
+    country_info = Country().one(name=countryName)
+    APIType = country_info.APIType
+    level = country_info.level
+    names = []
+    results = []
+    if APIType == 'basic':
+        if level == 'country':  # 国内各省
+            result = ChinaStatus.all(city='all')
+            for item in result:
+                if item.nowConfirm == 0:
+                    continue
+                names.append(item.province)
+                results.append({"name": item.province, "value": item.nowConfirm})
+        elif level == 'province':  # 省内各市
+            result = ChinaStatus.all(province=countryName)
+            for city_info in result:
+                if city_info.name != 'all':
+                    names.append(city_info.name)
+                    results.append({"name": city_info.city, "value": city_info.nowConfirm})
+    elif APIType == 'foreign':
+        if level == 'country':
+            result = GlobalStatus.all(country=countryName)
+            for pro_info in result:
+                if pro_info.province != 'all' and pro_info.nowConfirm >= 0:
+                    names.append(pro_info.province)
+                    results.append({"name": pro_info.province, "value": pro_info.nowConfirm})
+    else:
+        print("error province search name.")
+    return jsonify({"data": results, "name": names})
+
+
+@main.route('/barChartData', methods=['POST'])
+def getBarData():
+    countryName = request.form["name"]
+    results = []
+    results.append(['provinceName', 'confirm', 'dead', 'heal'])
+    name_info = Country().one(name=countryName)
+    APIType = name_info.APIType
+    if APIType == 'basic':
+        pro_info_list = ChinaStatus.all(city='all')
+    elif APIType == 'foreign':
+        pro_info_list = GlobalStatus().all(country=countryName)
+    else:
+        pro_info_list = []
+    for i in range(min(9, len(pro_info_list))):
+        results.append(
+            [pro_info_list[i].province, pro_info_list[i].confirm, pro_info_list[i].dead, pro_info_list[i].heal])
     return jsonify({"data": results})
 
 
 @main.route('/forecast')
 def forecast():
-    data = GlobalStatus.all(country='美国')
+    data = GlobalStatus.all(country='美国')  # 美国数据
     user = current_user()
     statis_data = {
         'time': data.updateTime,
@@ -88,6 +122,37 @@ def forecast():
     return render_template('dashboard/forecast.html', data=statis_data, u=user, name='美国')
 
 
-@main.route('/maps/')
+@main.route('/maps')
 def maps():
-    data = ChinaStatus.all(city='all')
+    data = ChinaStatus.one(province='all')  # 全国数据
+    user = current_user()
+    statis_data = {
+        'time': data.updateTime,
+        'name': '中国',
+        'data': {
+            'confirm': data.confirm,
+            'heal': data.heal,
+            'dead': data.dead,
+            'nowConfirm': data.nowConfirm
+        }
+    }
+    return render_template('dashboard/maps.html', data=statis_data, u=user, name='中国')
+
+
+@main.route('/mapChartData', methods=['POST'])
+def getMapData():
+    countryName = request.form["name"]
+    results = []
+    name_info = Country().one(name=countryName)
+    APIType = name_info.APIType
+    if APIType == 'basic':  # 全国疫情数据
+        pro_info_list = ChinaStatus.all(city='all')
+    elif APIType == 'foreign':  # 世界疫情数据，某国家疫情数据
+        pro_info_list = GlobalStatus().all(country=countryName)
+    else:
+        pro_info_list = []
+    for pro_infos in pro_info_list:
+        if pro_infos.province == 'all':
+            continue
+        results.append({'name': pro_infos.province, 'value': pro_infos.nowConfirm})
+    return jsonify({"data": results})
--- static/js/chart_control.js
+++ static/js/chart_control.js
@@ -1,22 +1,68 @@
-var myChart = echarts.init(document.getElementById('main'));
+var statis_chart_l = echarts.init(document.getElementById('chart-l'));
+var statis_chart_r = echarts.init(document.getElementById('chart-r'));
 
-$.get('data.json').done(function (data) {
-    myChart.setOption({
-        title: {
-            text: '异步数据加载示例'
-        },
-        tooltip: {},
-        legend: {
-            data:['销量']
-        },
-        xAxis: {
-            data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-        },
-        yAxis: {},
-        series: [{
-            name: '销量',
-            type: 'bar',
-            data: [5, 20, 36, 10, 10, 20]
-        }]
-    });
-});
+$.post('/dashboard/barChartData', {
+	"name": '{{ sd.country }}',
+}, ).done(function(data) {
+	var option = {
+		title: {
+			text: '疫情排名前8地区（湖北以外）',
+			subtext: '数据来自丁香园',
+		},
+		tooltip: {
+			trigger: 'axis',
+			axisPointer: {
+				type: 'shadow',
+			},
+		},
+		dataset: data,
+		grid: {
+			// 表格位置
+			left: '3%',
+			right: '4%',
+			bottom: '3%',
+			containLabel: true
+		},
+		xAxis: {
+			type: 'value',
+			// 表格中左右留出的宽度
+			boundaryGap: [0, 0.01],
+		},
+		yAxis: {
+			type: 'category',
+			data: []
+		},
+		series: [{
+				type: 'bar'
+			},
+			{
+				type: 'bar'
+			},
+			{
+				type: 'bar'
+			}
+		],
+	};
+	statis_chart_l.setOption(option);
+})
+
+$.post('/dashboard/pieChartData', {'name': '{{ sd.country }}'},).done(function(data) {
+			statis_chart_r.setOption({
+				title: {
+					text: '异步数据加载示例'
+				},
+				tooltip: {},
+				legend: {
+					data: ['销量']
+				},
+				xAxis: {
+					data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+				},
+				yAxis: {},
+				series: [{
+					name: '销量',
+					type: 'bar',
+					data: [5, 20, 36, 10, 10, 20]
+				}]
+			});
+		});
